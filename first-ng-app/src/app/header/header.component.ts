e32447bcea3ce0594042ccd2178ec36f12a9c352
Joynal Alam <joynalam67598@gmail.com>
2023-03-13 00:53:57 +0600

    bootstrap, selectors, data-binding - one way, interpolation, property binding, event binding.

--- first-ng-app/src/app/header/header.component.ts
+++ first-ng-app/src/app/header/header.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  slogan: string = "Shop anything";
+  source: string = "../../assets/img/shop_header.jpg";
 
+  getSlogan = () => {
+    return this.slogan;
+  }
 }
