04f33b4413321661a9eb9f4cc0dda9c50f81fbbb
Joynal Alam <joynalam67598@gmail.com>
2023-03-13 00:59:31 +0600

    two way data binding.

--- first-ng-app/src/app/search/search.component.ts
+++ first-ng-app/src/app/search/search.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent {
-  searchValue: string = "";
+  searchValue: string = "enter somethings";
   changeSearchValue = (eventData: Event) => {
     this.searchValue = (<HTMLInputElement>eventData.target).value;
   }
