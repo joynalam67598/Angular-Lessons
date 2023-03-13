64c65d832237a4a96e7a753354522eec83389060
joynal_alam <joynal.alam@enosisbd.com>
2023-03-13 13:57:24 +0600

    added child componet.

--- first-ng-app/src/app/app.module.ts
+++ first-ng-app/src/app/app.module.ts
@@ -2,29 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ContainerComponent } from './Container/container.component';
-import { NavComponent } from './nav/nav.component';
 import { HeaderComponent } from './header/header.component';
-import { NotificationComponent } from './notification/notification.component';
-import { SearchComponent } from './search/search.component';
 import { FormsModule } from '@angular/forms';
-import { ProductsComponent } from './products/products.component';
+import { CourseComponent } from './course/course.component';
+import { SearchComponent } from './search/search.component';
+import { FilterComponent } from './filter/filter.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ContainerComponent,
-    NavComponent,
-    HeaderComponent,
-    NotificationComponent,
-    SearchComponent,
-    ProductsComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent, HeaderComponent, CourseComponent, SearchComponent, FilterComponent],
+    imports: [BrowserModule, FormsModule],
+    providers: [],
+    bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- first-ng-app/src/app/search/search.component.ts
+++ first-ng-app/src/app/search/search.component.ts
@@ -1,13 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'app-search',
-    templateUrl: './search.component.html',
-    styleUrls: ['./search.component.css'],
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.css']
 })
 export class SearchComponent {
-    searchValue: string = '';
-    changeSearchValue = (eventData: Event) => {
-        this.searchValue = (<HTMLInputElement>eventData.target).value;
-    };
+
 }
