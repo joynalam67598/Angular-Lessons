da6855c44e80870771492c03097acfba033b2417
joynal_alam <joynal.alam@enosisbd.com>
2023-03-14 11:41:42 +0600

    fix merge error.

--- first-ng-app/src/app/demo/demo.component.ts
+++ first-ng-app/src/app/demo/demo.component.ts
@@ -1,8 +1,8 @@
-
 import {
     AfterContentInit,
     ContentChild,
     ElementRef,
+    Component,
 } from '@angular/core';
 
 @Component({
@@ -10,13 +10,11 @@ import {
     templateUrl: './demo.component.html',
     styleUrls: ['./demo.component.css'],
 })
-
 export class DemoComponent implements AfterContentInit {
-
     ngAfterContentInit(): void {
         this.paragraphElm.nativeElement.textContent = 'New value';
         console.log(this.paragraphElm);
     }
-    
+
     @ContentChild('para') paragraphElm!: ElementRef;
 }
