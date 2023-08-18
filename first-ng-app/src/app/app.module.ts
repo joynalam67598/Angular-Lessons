a76879937b324d813eca90f3ecf03677429f6a10
joynal_alam <joynal.alam@enosisbd.com>
2023-08-19 05:47:50 +0600

    Renderer2 Directive.

--- first-ng-app/src/app/app.module.ts
+++ first-ng-app/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { SetBackgroundDirective } from './CustomsDirectives/set-background.directive';
+import { HighlightDirective } from './CustomsDirectives/highlight.directive';
 
 @NgModule({
-    declarations: [AppComponent, SetBackgroundDirective],
+    declarations: [AppComponent, SetBackgroundDirective, HighlightDirective],
     imports: [BrowserModule, FormsModule],
     providers: [],
     bootstrap: [AppComponent],
