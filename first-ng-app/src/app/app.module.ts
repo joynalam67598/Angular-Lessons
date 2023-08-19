9de07dc5692d443d1f99413d2bfb832e0928a209
joynal_alam <joynal.alam@enosisbd.com>
2023-08-19 06:06:25 +0600

    HostListener Directive.

--- first-ng-app/src/app/app.module.ts
+++ first-ng-app/src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { SetBackgroundDirective } from './CustomsDirectives/set-background.directive';
 import { HighlightDirective } from './CustomsDirectives/highlight.directive';
+import { HoverDirective } from './CustomsDirectives/hover.directive';
 
 @NgModule({
-    declarations: [AppComponent, SetBackgroundDirective, HighlightDirective],
+    declarations: [AppComponent, SetBackgroundDirective, HighlightDirective, HoverDirective],
     imports: [BrowserModule, FormsModule],
     providers: [],
     bootstrap: [AppComponent],
